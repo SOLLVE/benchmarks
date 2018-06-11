3315266b3b7e716ad01871dcdc63836b15bd16de
Lingda Li <lildmh@gmail.com>
2018-06-11 17:45:24 -0400

    update hotspot gpu & um version

--- rodinia_3.1/openmp/hotspot/hotspot_openmp.cpp
+++ rodinia_3.1/openmp/hotspot/hotspot_openmp.cpp
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <omp.h>
 #include <sys/time.h>
+#include <cassert>
 
 // Returns the current system time in microseconds 
 long long get_time()
@@ -14,7 +15,8 @@ long long get_time()
 
 using namespace std;
 
-#define BLOCK_SIZE 16
+//#define BLOCK_SIZE 16
+#define BLOCK_SIZE 1
 #define BLOCK_SIZE_C BLOCK_SIZE
 #define BLOCK_SIZE_R BLOCK_SIZE
 
@@ -51,18 +53,22 @@ int num_omp_threads;
  * advances the solution of the discretized difference equations 
  * by one time step
  */
-void single_iteration(FLOAT *result, FLOAT *temp, FLOAT *power, int row, int col,
+void single_iteration(FLOAT *result, FLOAT *temp, FLOAT *power, unsigned long long row, unsigned long long col,
 					  FLOAT Cap_1, FLOAT Rx_1, FLOAT Ry_1, FLOAT Rz_1, 
 					  FLOAT step)
 {
     FLOAT delta;
-    int r, c;
-    int chunk;
-    int num_chunk = row*col / (BLOCK_SIZE_R * BLOCK_SIZE_C);
-    int chunks_in_row = col/BLOCK_SIZE_C;
-    int chunks_in_col = row/BLOCK_SIZE_R;
+    unsigned long long r, c;
+    unsigned long long chunk;
+    unsigned long long num_chunk = row*col / (BLOCK_SIZE_R * BLOCK_SIZE_C);
+    unsigned long long chunks_in_row = col/BLOCK_SIZE_C;
+    unsigned long long chunks_in_col = row/BLOCK_SIZE_R;
 
-#ifdef OPEN
+#if defined(OMP_GPU_OFFLOAD_UM)
+    #pragma omp target teams distribute parallel for private(chunk, r, c, delta) firstprivate(row, col, num_chunk, chunks_in_row)
+#elif defined(OMP_GPU_OFFLOAD)
+    #pragma omp target teams distribute parallel for private(chunk, r, c, delta) firstprivate(row, col, num_chunk, chunks_in_row)
+#elif defined(OPEN)
     #ifndef __MIC__
 	omp_set_num_threads(num_omp_threads);
     #endif
@@ -70,10 +76,10 @@ void single_iteration(FLOAT *result, FLOAT *temp, FLOAT *power, int row, int col
 #endif
     for ( chunk = 0; chunk < num_chunk; ++chunk )
     {
-        int r_start = BLOCK_SIZE_R*(chunk/chunks_in_col);
-        int c_start = BLOCK_SIZE_C*(chunk%chunks_in_row); 
-        int r_end = r_start + BLOCK_SIZE_R > row ? row : r_start + BLOCK_SIZE_R;
-        int c_end = c_start + BLOCK_SIZE_C > col ? col : c_start + BLOCK_SIZE_C;
+        unsigned long long r_start = BLOCK_SIZE_R*(chunk/chunks_in_col);
+        unsigned long long c_start = BLOCK_SIZE_C*(chunk%chunks_in_row); 
+        unsigned long long r_end = r_start + BLOCK_SIZE_R > row ? row : r_start + BLOCK_SIZE_R;
+        unsigned long long c_end = c_start + BLOCK_SIZE_C > col ? col : c_start + BLOCK_SIZE_C;
        
         if ( r_start == 0 || c_start == 0 || r_end == row || c_end == col )
         {
@@ -135,7 +141,9 @@ void single_iteration(FLOAT *result, FLOAT *temp, FLOAT *power, int row, int col
         }
 
         for ( r = r_start; r < r_start + BLOCK_SIZE_R; ++r ) {
+#if !defined(OMP_GPU_OFFLOAD) && !defined(OMP_GPU_OFFLOAD_UM)
 #pragma omp simd        
+#endif
             for ( c = c_start; c < c_start + BLOCK_SIZE_C; ++c ) {
             /* Update Temperatures */
                 result[r*col+c] =temp[r*col+c]+ 
@@ -148,6 +156,182 @@ void single_iteration(FLOAT *result, FLOAT *temp, FLOAT *power, int row, int col
     }
 }
 
+void single_iteration_gpu(FLOAT *result, FLOAT *temp, FLOAT *power, unsigned long long row, unsigned long long col,
+					  FLOAT Cap_1, FLOAT Rx_1, FLOAT Ry_1, FLOAT Rz_1, 
+					  FLOAT step)
+{
+    FLOAT delta;
+    unsigned long long r, c;
+    unsigned long long chunk;
+    unsigned long long num_chunk = row*col;
+
+#if defined(OMP_GPU_OFFLOAD_UM)
+    #pragma omp target teams distribute parallel for private(chunk, r, c, delta) firstprivate(row, col, num_chunk)
+#elif defined(OMP_GPU_OFFLOAD)
+    #pragma omp target teams distribute parallel for private(chunk, r, c, delta) firstprivate(row, col, num_chunk)
+#elif defined(OPEN)
+    #ifndef __MIC__
+	omp_set_num_threads(num_omp_threads);
+    #endif
+    #pragma omp parallel for shared(power, temp, result) private(chunk, r, c, delta) firstprivate(row, col, num_chunk) schedule(static)
+#endif
+    for ( chunk = 0; chunk < num_chunk; ++chunk )
+    {
+        r = chunk/col;
+        c = chunk%col; 
+       
+        if ( r == 0 || c == 0 || r+1 == row || c+1 == col )
+        {
+            /* Corner 1 */
+            if ( (r == 0) && (c == 0) ) {
+                delta = (Cap_1) * (power[0] +
+                    (temp[1] - temp[0]) * Rx_1 +
+                    (temp[col] - temp[0]) * Ry_1 +
+                    (amb_temp - temp[0]) * Rz_1);
+            }	/* Corner 2 */
+            else if ((r == 0) && (c == col-1)) {
+                delta = (Cap_1) * (power[c] +
+                    (temp[c-1] - temp[c]) * Rx_1 +
+                    (temp[c+col] - temp[c]) * Ry_1 +
+                (   amb_temp - temp[c]) * Rz_1);
+            }	/* Corner 3 */
+            else if ((r == row-1) && (c == col-1)) {
+                delta = (Cap_1) * (power[r*col+c] + 
+                    (temp[r*col+c-1] - temp[r*col+c]) * Rx_1 + 
+                    (temp[(r-1)*col+c] - temp[r*col+c]) * Ry_1 + 
+                (   amb_temp - temp[r*col+c]) * Rz_1);					
+            }	/* Corner 4	*/
+            else if ((r == row-1) && (c == 0)) {
+                delta = (Cap_1) * (power[r*col] + 
+                    (temp[r*col+1] - temp[r*col]) * Rx_1 + 
+                    (temp[(r-1)*col] - temp[r*col]) * Ry_1 + 
+                    (amb_temp - temp[r*col]) * Rz_1);
+            }	/* Edge 1 */
+            else if (r == 0) {
+                delta = (Cap_1) * (power[c] + 
+                    (temp[c+1] + temp[c-1] - 2.0*temp[c]) * Rx_1 + 
+                    (temp[col+c] - temp[c]) * Ry_1 + 
+                    (amb_temp - temp[c]) * Rz_1);
+            }	/* Edge 2 */
+            else if (c == col-1) {
+                delta = (Cap_1) * (power[r*col+c] + 
+                    (temp[(r+1)*col+c] + temp[(r-1)*col+c] - 2.0*temp[r*col+c]) * Ry_1 + 
+                    (temp[r*col+c-1] - temp[r*col+c]) * Rx_1 + 
+                    (amb_temp - temp[r*col+c]) * Rz_1);
+            }	/* Edge 3 */
+            else if (r == row-1) {
+                delta = (Cap_1) * (power[r*col+c] + 
+                    (temp[r*col+c+1] + temp[r*col+c-1] - 2.0*temp[r*col+c]) * Rx_1 + 
+                    (temp[(r-1)*col+c] - temp[r*col+c]) * Ry_1 + 
+                    (amb_temp - temp[r*col+c]) * Rz_1);
+            }	/* Edge 4 */
+            else if (c == 0) {
+                delta = (Cap_1) * (power[r*col] + 
+                    (temp[(r+1)*col] + temp[(r-1)*col] - 2.0*temp[r*col]) * Ry_1 + 
+                    (temp[r*col+1] - temp[r*col]) * Rx_1 + 
+                    (amb_temp - temp[r*col]) * Rz_1);
+            }
+            result[r*col+c] =temp[r*col+c]+ delta;
+        }
+        else {
+            /* Update Temperatures */
+            result[r*col+c] =temp[r*col+c]+ 
+                 ( Cap_1 * (power[r*col+c] + 
+                (temp[(r+1)*col+c] + temp[(r-1)*col+c] - 2.f*temp[r*col+c]) * Ry_1 + 
+                (temp[r*col+c+1] + temp[r*col+c-1] - 2.f*temp[r*col+c]) * Rx_1 + 
+                (amb_temp - temp[r*col+c]) * Rz_1));
+        }
+    }
+}
+
+void single_iteration_gpu2(FLOAT *result, FLOAT *temp, FLOAT *power, unsigned long long row, unsigned long long col,
+					  FLOAT Cap_1, FLOAT Rx_1, FLOAT Ry_1, FLOAT Rz_1, 
+					  FLOAT step)
+{
+    FLOAT delta;
+    unsigned long long r, c;
+    unsigned long long chunk;
+    unsigned long long num_chunk = row*col;
+
+#if defined(OMP_GPU_OFFLOAD_UM)
+    #pragma omp target teams distribute parallel for private(chunk, r, c, delta) firstprivate(row, col, num_chunk)
+#elif defined(OMP_GPU_OFFLOAD)
+    #pragma omp target teams distribute parallel for private(chunk, r, c, delta) firstprivate(row, col, num_chunk)
+#elif defined(OPEN)
+    #ifndef __MIC__
+	omp_set_num_threads(num_omp_threads);
+    #endif
+    #pragma omp parallel for shared(power, temp, result) private(chunk, r, c, delta) firstprivate(row, col, num_chunk) schedule(static)
+#endif
+    for ( chunk = 0; chunk < num_chunk; ++chunk )
+    {
+        r = chunk/col;
+        c = chunk%col; 
+       
+        if ( r == 0 || c == 0 || r+1 == row || c+1 == col )
+        {
+            /* Corner 1 */
+            if ( (r == 0) && (c == 0) ) {
+                delta = (Cap_1) * (power[0] +
+                    (temp[1] - temp[0]) * Rx_1 +
+                    (temp[col] - temp[0]) * Ry_1 +
+                    (amb_temp - temp[0]) * Rz_1);
+            }	/* Corner 2 */
+            else if ((r == 0) && (c == col-1)) {
+                delta = (Cap_1) * (power[c] +
+                    (temp[c-1] - temp[c]) * Rx_1 +
+                    (temp[c+col] - temp[c]) * Ry_1 +
+                (   amb_temp - temp[c]) * Rz_1);
+            }	/* Corner 3 */
+            else if ((r == row-1) && (c == col-1)) {
+                delta = (Cap_1) * (power[r*col+c] + 
+                    (temp[r*col+c-1] - temp[r*col+c]) * Rx_1 + 
+                    (temp[(r-1)*col+c] - temp[r*col+c]) * Ry_1 + 
+                (   amb_temp - temp[r*col+c]) * Rz_1);					
+            }	/* Corner 4	*/
+            else if ((r == row-1) && (c == 0)) {
+                delta = (Cap_1) * (power[r*col] + 
+                    (temp[r*col+1] - temp[r*col]) * Rx_1 + 
+                    (temp[(r-1)*col] - temp[r*col]) * Ry_1 + 
+                    (amb_temp - temp[r*col]) * Rz_1);
+            }	/* Edge 1 */
+            else if (r == 0) {
+                delta = (Cap_1) * (power[c] + 
+                    (temp[c+1] + temp[c-1] - 2.0*temp[c]) * Rx_1 + 
+                    (temp[col+c] - temp[c]) * Ry_1 + 
+                    (amb_temp - temp[c]) * Rz_1);
+            }	/* Edge 2 */
+            else if (c == col-1) {
+                delta = (Cap_1) * (power[r*col+c] + 
+                    (temp[(r+1)*col+c] + temp[(r-1)*col+c] - 2.0*temp[r*col+c]) * Ry_1 + 
+                    (temp[r*col+c-1] - temp[r*col+c]) * Rx_1 + 
+                    (amb_temp - temp[r*col+c]) * Rz_1);
+            }	/* Edge 3 */
+            else if (r == row-1) {
+                delta = (Cap_1) * (power[r*col+c] + 
+                    (temp[r*col+c+1] + temp[r*col+c-1] - 2.0*temp[r*col+c]) * Rx_1 + 
+                    (temp[(r-1)*col+c] - temp[r*col+c]) * Ry_1 + 
+                    (amb_temp - temp[r*col+c]) * Rz_1);
+            }	/* Edge 4 */
+            else if (c == 0) {
+                delta = (Cap_1) * (power[r*col] + 
+                    (temp[(r+1)*col] + temp[(r-1)*col] - 2.0*temp[r*col]) * Ry_1 + 
+                    (temp[r*col+1] - temp[r*col]) * Rx_1 + 
+                    (amb_temp - temp[r*col]) * Rz_1);
+            }
+            result[r*col+c] =temp[r*col+c]+ delta;
+        }
+        else {
+            /* Update Temperatures */
+            result[r*col+c] =temp[r*col+c]+ 
+                 ( Cap_1 * (power[r*col+c] + 
+                (temp[(r+1)*col+c] + temp[(r-1)*col+c] - 2.f*temp[r*col+c]) * Ry_1 + 
+                (temp[r*col+c+1] + temp[r*col+c-1] - 2.f*temp[r*col+c]) * Rx_1 + 
+                (amb_temp - temp[r*col+c]) * Rz_1));
+        }
+    }
+}
+
 #ifdef OMP_OFFLOAD
 #pragma offload_attribute(pop)
 #endif
@@ -156,7 +340,7 @@ void single_iteration(FLOAT *result, FLOAT *temp, FLOAT *power, int row, int col
  * transfer differential equations to difference equations 
  * and solves the difference equations by iterating
  */
-void compute_tran_temp(FLOAT *result, int num_iterations, FLOAT *temp, FLOAT *power, int row, int col) 
+void compute_tran_temp(FLOAT *result, int num_iterations, FLOAT *temp, FLOAT *power, unsigned long long row, unsigned long long col) 
 {
 	#ifdef VERBOSE
 	int i = 0;
@@ -182,25 +366,44 @@ void compute_tran_temp(FLOAT *result, int num_iterations, FLOAT *temp, FLOAT *po
 	fprintf(stdout, "Rx: %g\tRy: %g\tRz: %g\tCap: %g\n", Rx, Ry, Rz, Cap);
 	#endif
 
-#ifdef OMP_OFFLOAD
-        int array_size = row*col;
+        unsigned long long array_size = row*col;
+#if defined(OMP_GPU_OFFLOAD_UM)
+#pragma omp target data \
+        map(temp[0:array_size]) \
+        map(to: power[0:array_size], row, col, Cap_1, Rx_1, Ry_1, Rz_1) \
+        map(from: result[0:array_size])
+#elif defined(OMP_GPU_OFFLOAD)
+#pragma omp target data \
+        map(temp[0:array_size]) \
+        map(to: power[0:array_size], row, col, Cap_1, Rx_1, Ry_1, Rz_1) \
+        map(from: result[0:array_size])
+#elif defined(OMP_OFFLOAD)
 #pragma omp target \
         map(temp[0:array_size]) \
-        map(to: power[0:array_size], row, col, Cap_1, Rx_1, Ry_1, Rz_1, step, num_iterations) \
+        map(to: power[0:array_size], row, col, Cap_1, Rx_1, Ry_1, Rz_1) \
         map( result[0:array_size])
 #endif
         {
+#if !defined(OMP_GPU_OFFLOAD_UM) && !defined(OMP_GPU_OFFLOAD)
             FLOAT* r = result;
             FLOAT* t = temp;
+#endif
             for (int i = 0; i < num_iterations ; i++)
             {
                 #ifdef VERBOSE
                 fprintf(stdout, "iteration %d\n", i++);
                 #endif
+#if defined(OMP_GPU_OFFLOAD_UM) || defined(OMP_GPU_OFFLOAD)
+                if (i % 2 == 0)
+                    single_iteration_gpu(result, temp, power, row, col, Cap_1, Rx_1, Ry_1, Rz_1, step);
+                else
+                    single_iteration_gpu2(temp, result, power, row, col, Cap_1, Rx_1, Ry_1, Rz_1, step);
+#else
                 single_iteration(r, t, power, row, col, Cap_1, Rx_1, Ry_1, Rz_1, step);
                 FLOAT* tmp = t;
                 t = r;
                 r = tmp;
+#endif
             }	
         }
 	#ifdef VERBOSE
@@ -274,7 +477,9 @@ void usage(int argc, char **argv)
 
 int main(int argc, char **argv)
 {
-	int grid_rows, grid_cols, sim_time, i;
+	//int grid_rows, grid_cols, sim_time, i;
+	int sim_time;
+	unsigned long long grid_rows, grid_cols, i;
 	FLOAT *temp, *power, *result;
 	char *tfile, *pfile, *ofile;
 	
@@ -288,10 +493,18 @@ int main(int argc, char **argv)
 		)
 		usage(argc, argv);
 
+    assert(num_omp_threads > 0);
+    unsigned long long input_time = num_omp_threads;
 	/* allocate memory for the temperature and power arrays	*/
-	temp = (FLOAT *) calloc (grid_rows * grid_cols, sizeof(FLOAT));
-	power = (FLOAT *) calloc (grid_rows * grid_cols, sizeof(FLOAT));
-	result = (FLOAT *) calloc (grid_rows * grid_cols, sizeof(FLOAT));
+#if defined(OMP_GPU_OFFLOAD_UM)
+	temp = (FLOAT *) omp_target_alloc (grid_rows * grid_cols * input_time * sizeof(FLOAT), -100);
+	power = (FLOAT *) omp_target_alloc (grid_rows * grid_cols * input_time * sizeof(FLOAT), -100);
+	result = (FLOAT *) omp_target_alloc (grid_rows * grid_cols * input_time * sizeof(FLOAT), -100);
+#else
+	temp = (FLOAT *) calloc (grid_rows * grid_cols * input_time, sizeof(FLOAT));
+	power = (FLOAT *) calloc (grid_rows * grid_cols * input_time, sizeof(FLOAT));
+	result = (FLOAT *) calloc (grid_rows * grid_cols * input_time, sizeof(FLOAT));
+#endif
 	if(!temp || !power)
 		fatal("unable to allocate memory");
 
@@ -303,7 +516,18 @@ int main(int argc, char **argv)
 	read_input(temp, grid_rows, grid_cols, tfile);
 	read_input(power, grid_rows, grid_cols, pfile);
 
-	printf("Start computing the transient temperature\n");
+    // enlarge input
+    for (unsigned long long t = 1; t < input_time; t++) {
+        for (i=0; i < grid_rows * grid_cols; i++) {
+            temp[t*grid_rows * grid_cols + i] = temp[i];
+            power[t*grid_rows * grid_cols + i] = power[i];
+        }
+    }
+    grid_rows *= input_time;
+
+	//printf("Start computing the transient temperature\n");
+    unsigned long long total_size = 3 * sizeof(FLOAT) * grid_rows * grid_cols;
+    printf("Size: %f GB\n", ((double)total_size) / (1024*1024*1024));
 	
     long long start_time = get_time();
 
@@ -311,10 +535,10 @@ int main(int argc, char **argv)
 
     long long end_time = get_time();
 
-    printf("Ending simulation\n");
-    printf("Total time: %.3f seconds\n", ((float) (end_time - start_time)) / (1000*1000));
+    //printf("Ending simulation\n");
+    printf("Total time: %f seconds\n", ((float) (end_time - start_time)) / (1000*1000));
 
-    writeoutput((1&sim_time) ? result : temp, grid_rows, grid_cols, ofile);
+    //writeoutput((1&sim_time) ? result : temp, grid_rows, grid_cols, ofile);
 
 	/* output results	*/
 #ifdef VERBOSE
@@ -326,8 +550,13 @@ int main(int argc, char **argv)
 	fprintf(stdout, "%d\t%g\n", i, temp[i]);
 #endif
 	/* cleanup	*/
+#if defined(OMP_GPU_OFFLOAD_UM)
+    omp_target_free(temp, omp_get_default_device());
+    omp_target_free(power, omp_get_default_device());
+#else
 	free(temp);
 	free(power);
+#endif
 
 	return 0;
 }
