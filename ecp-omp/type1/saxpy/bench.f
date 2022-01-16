9ff1d288fb67d3003dfa75c3859a5f375519d417
Vivek Kale <11766050+vlkale@users.noreply.github.com>
2022-01-16 17:50:27 -0500

    Update OpenMP offload

--- ecp-omp/type1/saxpy/bench.f
+++ ecp-omp/type1/saxpy/bench.f
@@ -1,23 +1,29 @@
 program main
-
       include 'omp_lib.h'
-
+     
+      use iso_fortran_env 
       integer i, n
       double precision x(1000), y(1000), s
-
+c     real(kind=real32), dimension(n) :: x
+c     real(kind=real32), dimension(n) :: y
+      
+      integer :: n, i, num_blocks
+      real(kind=real32) :: a 
+     
       n = 1000
       s = 123.456
-
+      
       do i = 1, n
         x(i) = rand ( )
         y(i) = rand ( )
       end do
-
-c$omp parallel do
+      
+      
+c$omp target teams distribute parallel simd map(to: x, a) map(tofrom: y) num_teams(num_blocks) thread_limit(112) do
       do i = 1, n
         y(i) = y(i) + s * x(i)
       end do
-c$omp end parallel do
+c$omp end teams distribute parallel
 
       stop
       end
