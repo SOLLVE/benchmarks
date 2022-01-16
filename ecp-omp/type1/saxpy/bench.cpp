9a009769cdfd0c7f68cac899a0a00a0ce5dd0336
Vivek Kale <11766050+vlkale@users.noreply.github.com>
2022-01-16 15:40:55 -0500

    Update bench.cpp

--- ecp-omp/type1/saxpy/bench.cpp
+++ ecp-omp/type1/saxpy/bench.cpp
@@ -1,24 +1,24 @@
+# include <stdlib.h>
+# include <stdio.h>
+# include <omp.h>
 
-      program main
+int main ( int argc, char *argv[] )
+{
+  int i, n = 1000;
+  double x[1000], y[1000], s;
 
-      include 'omp_lib.h'
+  s = 123.456;
 
-      integer i, n
-      double precision x(1000), y(1000), s
+  for ( i = 0; i < n; i++ )
+  {
+    x[i] = ( double ) rand ( ) / ( double ) RAND_MAX;
+    y[i] = ( double ) rand ( ) / ( double ) RAND_MAX;
+  }
 
-      n = 1000
-      s = 123.456
-
-      do i = 1, n
-        x(i) = rand ( )
-        y(i) = rand ( )
-      end do
-
-c$omp parallel do
-      do i = 1, n
-        y(i) = y(i) + s * x(i)
-      end do
-c$omp end parallel do
-
-      stop
-      end
+#pragma omp parallel for
+  for ( i = 0; i < n; i++ )
+  {
+    y[i] = y[i] + s * x[i];
+  }
+  return 0;
+}
