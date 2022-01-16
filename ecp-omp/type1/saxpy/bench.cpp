7bcdf316f0192ea78f0985e3ce21f4e5f0df0de5
Vivek Kale <11766050+vlkale@users.noreply.github.com>
2022-01-16 17:09:08 -0500

    Update bench.cpp

--- ecp-omp/type1/saxpy/bench.cpp
+++ ecp-omp/type1/saxpy/bench.cpp
@@ -15,10 +15,27 @@ int main ( int argc, char *argv[] )
     y[i] = ( double ) rand ( ) / ( double ) RAND_MAX;
   }
 
-#pragma omp parallel for
+//  void saxpy ( int n, float a, float* restrict x, float* restrict* y)
+ #ifdef DEFAULT   
+ #pragma omp teams distribute parallel for map(to: x[0:n], a) 
+  
+#elif TEAMS 
+  #pragma omp target teams distribute parallel for map(to: x[0:n], a) map(tofrom: y[0:n]) map(tofrom: y[0:n]) 
+ 
+#elif  THREADS
+  #pragma omp target teams distribute parallel for map(to: x[0:n], a) map(tofrom: y[0:n]) map(tofrom: y[0:n]) thread_limit(112) 
+  
+#elif TEAMS_THREADS 
+  #pragma omp target teams distribute parallel for map(to: x[0:n], a) map(tofrom: y[0:n]) map(tofrom: y[0:n]) num_teams(4) thread_limit(112) 
+  
+  #else   
+  #pragma omp parallel for  
+  #endif 
+   
   for ( i = 0; i < n; i++ )
   {
     y[i] = y[i] + s * x[i];
-  }
+  } 
+  
   return 0;
 }
